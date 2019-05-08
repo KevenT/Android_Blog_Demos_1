d9cece69b51320da69301f263c0c83e88e254e25
Keven_Company <tv2014@126.com>
2019-05-08 16:54:10 +0800

    适配as3.4 提交。待解决p9大图加载不出来图片尺寸问题

---
+++ blogcodes/src/main/java/com/zhy/blogcodes/largeImage/LargeImageViewActivity.java
@@ -0,0 +1,40 @@
+package com.zhy.blogcodes.largeImage;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhy.blogcodes.R;
+import com.zhy.blogcodes.largeImage.view.LargeImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LargeImageViewActivity extends AppCompatActivity
+{
+    private LargeImageView mLargeImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_large_image_view);
+
+        mLargeImageView = (LargeImageView) findViewById(R.id.id_largetImageview);
+        try
+        {
+
+            InputStream inputStream = getAssets().open("qm.jpg");
+//            InputStream inputStream = getAssets().open("tangyan.jpg");
+
+                mLargeImageView.setInputStream(inputStream);
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
